ddf2713fe0688ec47b6c4393158d1e6bd2c1161d
yannick grenzinger <yannick.grenzinger@gmail.com>
2014-06-16 22:42:39 +0200

    React implem

--- public/js/directives.js
+++ public/js/directives.js
@@ -2,25 +2,63 @@
 
 /* Directives */
 
+
+var MYSTOCKS = React.createClass({
+  displayName: 'MYSTOCKS',
+  render: function() {
+
+    var stocks = this.props.stocks;
+    if (stocks.length > 0) {
+      var rows = stocks.map(function(stock) {
+        var classString
+        var clickHandler = function(ev) {
+          console.log("Still in reactJs");
+          console.log(ev);
+        }
+
+        return (
+          React.DOM.tr({
+              onClick: clickHandler
+            },
+            React.DOM.td(null, stock['symbol']),
+            React.DOM.td(null, stock['price']),
+            React.DOM.td(null, stock['variation']),
+            React.DOM.td(null, stock['company']),
+            React.DOM.td(null, stock['sector'])
+          )
+        );
+      });
+    }
+
+    return (
+      React.DOM.table(null,
+        rows
+      )
+    );
+  }
+});
+
 angular.module('myApp.directives', [])
-  .directive('appVersion', function (appname) {
+  .directive('appVersion', function(appname) {
     return function(scope, elm, attrs) {
       elm.text(appname);
     };
   })
-  .directive('fastRepeat', function(){
-      return{
-          restrict: 'E',
-          scope:{
-              data: '='
-          },
-          link:function(scope, el, attrs){
-              scope.$watch('data', function(newValue, oldValue){
-                  React.renderComponent(
-                      MYLIST({data:newValue}),
-                      el[0]
-                  );
-              })
-          }
+  .directive('fastStocks', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        stocks: '='
+      },
+      link: function(scope, el, attrs) {
+        scope.$watchCollection('stocks', function(newValue, oldValue) {
+          React.renderComponent(
+            MYSTOCKS({
+              stocks: newValue
+            }),
+            el[0]
+          );
+        })
       }
+    }
   });
